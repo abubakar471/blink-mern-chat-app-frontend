3071cbd7f5f712c29ceee2fe234ef633620c365e
abubakar471 <abdurjoy2001@gmail.com>
2023-08-31 11:40:29 +0600

    styling of default navbar

--- src/components/default-navbar.jsx
+++ src/components/default-navbar.jsx
@@ -6,7 +6,7 @@ const DefaultNavbar = () => {
         <div style={{ Index: "5555" }} className="p-4 border-b">
             <nav className="flex items-center justify-between">
                 <Logo />
-                <div className="flex items-center gap-2 md:pr-10">
+                <div className="flex items-center gap-4 md:gap-2 lg:gap-2 md:pr-10">
                     <NavLink className="border-b-2 border-t-2 border-violet-500 
                     hover:bg-violet-500 hover:text-white rounded-lg p-1 transition-all ease-in" 
                     to="/register">Register</NavLink>
